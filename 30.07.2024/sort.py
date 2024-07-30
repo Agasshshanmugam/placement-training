ed36934d444a34b892fbf73ba88e97c8c25f9ca5
Agassh <151323244+Agasshshanmugam@users.noreply.github.com>
2024-07-30 19:44:56 +0530

    Create sort.py

---
+++ 30.07.2024/sort.py
@@ -0,0 +1,11 @@
+def main():
+    numbers = [5, 2, 9, 1, 5, 6]
+    numbers.sort()
+    print("Sorted list using sort():", numbers)
+
+    numbers = [5, 2, 9, 1, 5, 6]
+    sorted_numbers = sorted(numbers)
+    print("Sorted list using sorted():", sorted_numbers)
+
+if __name__ == "__main__":
+    main()
