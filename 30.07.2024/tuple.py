bbbcba2712be93ee35038f82b2c170c80744f036
Agassh <151323244+Agasshshanmugam@users.noreply.github.com>
2024-07-30 19:39:30 +0530

    Create tuple.py

---
+++ 30.07.2024/tuple.py
@@ -0,0 +1,25 @@
+my_tuple = (1, 2, 3, 4, 5)
+
+first_element = my_tuple[0]
+last_element = my_tuple[-1]
+
+sub_tuple = my_tuple[1:4]
+
+new_tuple = my_tuple + (6, 7, 8)
+
+repeated_tuple = my_tuple * 2
+
+is_three_in_tuple = 3 in my_tuple
+
+index_of_four = my_tuple.index(4)
+
+count_of_twos = my_tuple.count(2)
+
+print("First element:", first_element)
+print("Last element:", last_element)
+print("Sliced tuple:", sub_tuple)
+print("Concatenated tuple:", new_tuple)
+print("Repeated tuple:", repeated_tuple)
+print("Is 3 in tuple?", is_three_in_tuple)
+print("Index of 4:", index_of_four)
+print("Count of 2s:", count_of_twos)
